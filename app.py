a270611c6856e2508cae9422ea11d871cbd17e6a
Andre Saddler <dresadd09691@gmail.com>
2023-05-15 01:36:29 -0400

    refactor(app.py): refactor code to improve readability and maintainability feat(app.py): add error handling for file selection and missing input fields feat(app.py): add support for output format selection feat(app.py): add support for precision selection feat(app.py): add support for merging method selection feat(app.py): add support for weights and bases selection feat(app.py): add support for saving merged model to file
    chore: update requirements.txt file

--- app.py
+++ app.py
@@ -1,130 +1,179 @@
-import os
 import tkinter as tk
-from tkinter import filedialog
-from sd_meh.merge import merge_models
+from tkinter import ttk, filedialog, messagebox
+from sd_meh.merge import NUM_TOTAL_BLOCKS, merge_models, save_model
+import os
 
 
-def select_model_a():
-    global model_a_path
-    model_a_path = filedialog.askopenfilename(title="Select Model A")
-    model_a_entry.delete(0, tk.END)
-    model_a_entry.insert(0, os.path.basename(model_a_path))
+def browse_model(entry):
+    file_path = filedialog.askopenfilename()
+    if not os.access(file_path, os.R_OK):
+        messagebox.showerror("Error", f"Cannot read the file: {file_path}")
+        return
+    entry.delete(0, tk.END)
+    entry.insert(0, file_path)
 
 
-def select_model_b():
-    global model_b_path
-    model_b_path = filedialog.askopenfilename(title="Select Model B")
-    model_b_entry.delete(0, tk.END)
-    model_b_entry.insert(0, os.path.basename(model_b_path))
+def compute_weights(weights, base):
+    if not weights:
+        return [base] * NUM_TOTAL_BLOCKS
+    if "," in weights:
+        w_alpha = list(map(float, weights.split(",")))
+        if len(w_alpha) == NUM_TOTAL_BLOCKS:
+            return w_alpha
 
 
-def select_model_c():
-    global model_c_path
-    model_c_path = filedialog.askopenfilename(title="Select Model C")
-    model_c_entry.delete(0, tk.END)
-    model_c_entry.insert(0, os.path.basename(model_c_path))
+def on_merge_click():
+    # Read values from input fields
+    model_a = model_a_entry.get()
+    model_b = model_b_entry.get()
+    model_c = model_c_entry.get()
+    merge_mode = merging_method_var.get()
+    precision = precision_var.get()
+    output_path = output_path_entry.get()
+    output_format = output_format_var.get()
+    weights_alpha = weights_alpha_entry.get()
+    base_alpha = float(base_alpha_entry.get())
+    weights_beta = weights_beta_entry.get()
+    base_beta = float(base_beta_entry.get())
 
+    if not model_a or not model_b:
+        messagebox.showerror("Error", "Please provide paths for Model A and Model B.")
+        return
 
-def merge():
-    models = {"model_a": model_a_path, "model_b": model_b_path}
-    if model_c_path:
-        models["model_c"] = model_c_path
+    if merge_mode == "add_difference" and not model_c:
+        messagebox.showerror(
+            "Error",
+            "Please provide a path for Model C when using the 'add_difference' merging method.",
+        )
+        return
 
-    weight_alpha_values = [float(x) for x in weight_var_a.get().split(",") if x.strip()]
-    weight_beta_values = [float(x) for x in weight_var_b.get().split(",") if x.strip()]
+    # Create the models dictionary conditionally
+    models = {"model_a": model_a, "model_b": model_b}
+    if merge_mode == "add_difference":
+        models["model_c"] = model_c
 
+    # Call the modified main function with the appropriate arguments
     merged_model = merge_models(
-        models,
-        weights={"alpha": weight_alpha_values, "beta": weight_beta_values},
-        bases={"alpha": base_var_a.get(), "beta": base_var_b.get()},
-        merge_mode=merge_mode_var.get(),
-        precision=precision_var.get(),
+        models=models,  # Use the updated models dictionary
+        weights={
+            "alpha": compute_weights(weights_alpha, base_alpha),
+            "beta": compute_weights(weights_beta, base_beta),
+        },
+        bases={"alpha": base_alpha, "beta": base_beta},
+        merge_mode=merge_mode,
+        precision=precision,
     )
+    save_model(merged_model, output_path, output_format)
 
-    output_path = filedialog.asksaveasfilename(
-        title="Save Merged Model", defaultextension=".ckpt"
-    )
-    merged_model.save(output_path)
+    messagebox.showinfo("Success", "Models merged successfully!")
 
 
+def create_file_input(row, label_text):
+    label = ttk.Label(root, text=label_text)
+    label.grid(column=0, row=row)
+    entry = ttk.Entry(root)
+    entry.grid(column=1, row=row)
+    browse_button = ttk.Button(root, text="Browse", command=lambda: browse_model(entry))
+    browse_button.grid(column=2, row=row)
+    return entry
+
+
+# Create the main window
 root = tk.Tk()
-root.title("MEH - Merging Execution Helper")
+root.title("Stable Diffusion Model Merging")
 
-model_a_path = None
-model_b_path = None
-model_c_path = None
+# Add input fields, labels, and other widgets
+row = 0
 
-weight_var_a = tk.StringVar()
-weight_var_b = tk.StringVar()
+model_a_entry = create_file_input(row, "Model A")
+row += 1
 
-base_var_a = tk.DoubleVar()
-base_var_b = tk.DoubleVar()
+model_b_entry = create_file_input(row, "Model B")
+row += 1
 
-merge_mode_var = tk.StringVar()
-merge_mode_var.set("weighted_sum")
+model_c_entry = create_file_input(row, "Model C (optional)")
+row += 1
 
-precision_var = tk.IntVar()
-precision_var.set(32)
-
-merge_modes = [
-    "weighted_sum",
-    "add_difference",
-    "weighted_subtraction",
-    "sum_twice",
-    "triple_sum",
-    "tensor_sum",
-]
-
-merge_mode_dropdown = tk.OptionMenu(root, merge_mode_var, *merge_modes)
-merge_mode_dropdown.grid(row=0, column=0, columnspan=2)
-
-precision_options = {"fp16": 16, "fp32": 32}
-precision_var.set(precision_options["fp32"])
-
-precision_dropdown = tk.OptionMenu(root, precision_var, *precision_options.values())
-precision_dropdown.grid(row=1, column=0, columnspan=2)
-
-select_model_a_button = tk.Button(root, text="Select Model A", command=select_model_a)
-select_model_a_button.grid(row=2, column=0)
-
-model_a_entry = tk.Entry(root)
-model_a_entry.grid(row=2, column=1)
-
-select_model_b_button = tk.Button(root, text="Select Model B", command=select_model_b)
-select_model_b_button.grid(row=3, column=0)
-
-model_b_entry = tk.Entry(root)
-model_b_entry.grid(row=3, column=1)
-
-select_model_c_button = tk.Button(
-    root, text="Select Model C (Optional)", command=select_model_c
+merging_method_label = ttk.Label(root, text="Merging Method")
+merging_method_label.grid(column=0, row=row)
+merging_method_var = tk.StringVar()
+merging_method_combobox = ttk.Combobox(
+    root,
+    textvariable=merging_method_var,
+    values=[
+        "weighted_sum",
+        "add_difference",
+        "weighted_subtraction",
+        "sum_twice",
+        "triple_sum",
+        "tensor_sum",
+    ],
 )
-select_model_c_button.grid(row=4, column=0)
+merging_method_combobox.grid(column=1, row=row)
+merging_method_combobox.current(0)
 
-model_c_entry = tk.Entry(root)
-model_c_entry.grid(row=4, column=1)
+row += 1
 
-weight_alpha_label = tk.Label(root, text="Weight Alpha:")
-weight_alpha_label.grid(row=5, column=0)
-weight_alpha_entry = tk.Entry(root, textvariable=weight_var_a)
-weight_alpha_entry.grid(row=5, column=1)
+precision_label = ttk.Label(root, text="Precision")
+precision_label.grid(column=0, row=row)
+precision_var = tk.IntVar(value=16)
+precision_combobox = ttk.Combobox(root, textvariable=precision_var, values=[16, 32])
+precision_combobox.grid(column=1, row=row)
+precision_combobox.current(0)
 
-base_alpha_label = tk.Label(root, text="Base Alpha:")
-base_alpha_label.grid(row=6, column=0)
-base_alpha_entry = tk.Entry(root, textvariable=base_var_a)
-base_alpha_entry.grid(row=6, column=1)
+row += 1
 
-weight_beta_label = tk.Label(root, text="Weight Beta:")
-weight_beta_label.grid(row=7, column=0)
-weight_beta_entry = tk.Entry(root, textvariable=weight_var_b)
-weight_beta_entry.grid(row=7, column=1)
+output_path_label = ttk.Label(root, text="Output Path")
+output_path_label.grid(column=0, row=row)
+output_path_entry = ttk.Entry(root)
+output_path_entry.grid(column=1, row=row)
+output_path_entry.insert(0, "model_out")
 
-base_beta_label = tk.Label(root, text="Base Beta:")
-base_beta_label.grid(row=8, column=0)
-base_beta_entry = tk.Entry(root, textvariable=base_var_b)
-base_beta_entry.grid(row=8, column=1)
+row += 1
 
-merge_button = tk.Button(root, text="Merge Models", command=merge)
-merge_button.grid(row=9, column=0, columnspan=2)
+output_format_label = ttk.Label(root, text="Output Format")
+output_format_label.grid(column=0, row=row)
+output_format_var = tk.StringVar()
+output_format_combobox = ttk.Combobox(
+    root, textvariable=output_format_var, values=["safetensors", "ckpt"]
+)
+output_format_combobox.grid(column=1, row=row)
+output_format_combobox.current(0)
 
+row += 1
+
+weights_alpha_label = ttk.Label(root, text="Weights Alpha")
+weights_alpha_label.grid(column=0, row=row)
+weights_alpha_entry = ttk.Entry(root)
+weights_alpha_entry.grid(column=1, row=row)
+
+row += 1
+
+base_alpha_label = ttk.Label(root, text="Base Alpha")
+base_alpha_label.grid(column=0, row=row)
+base_alpha_entry = ttk.Entry(root)
+base_alpha_entry.grid(column=1, row=row)
+base_alpha_entry.insert(0, "0.0")
+
+row += 1
+
+weights_beta_label = ttk.Label(root, text="Weights Beta")
+weights_beta_label.grid(column=0, row=row)
+weights_beta_entry = ttk.Entry(root)
+weights_beta_entry.grid(column=1, row=row)
+
+row += 1
+
+base_beta_label = ttk.Label(root, text="Base Beta")
+base_beta_label.grid(column=0, row=row)
+base_beta_entry = ttk.Entry(root)
+base_beta_entry.grid(column=1, row=row)
+base_beta_entry.insert(0, "0.0")
+
+row += 1
+
+merge_button = ttk.Button(root, text="Merge Models", command=on_merge_click)
+merge_button.grid(column=1, row=row)
+
+# Start the main event loop
 root.mainloop()
