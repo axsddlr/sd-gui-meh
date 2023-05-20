b77b63a31bd81d4c4b7a554ea0114b89da8cf92e
Andre Saddler <dresadd09691@gmail.com>
2023-05-20 03:20:46 -0400

    feat(app.py): add a new tab to the GUI for displaying information about the application feat(about.py): create a new class for managing hyperlinks and add a new tab to the GUI for displaying information about the application The new tab in the GUI displays information about the application, including the version, author, and a hyperlink to the GitHub repository. The hyperlink is managed by a new class, HyperlinkManager, which handles the hyperlink's styling and behavior. The About class is responsible for creating the frame and adding the necessary labels and hyperlink.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from sd_meh.merge import NUM_TOTAL_BLOCKS, merge_models, save_model
 from settings import Config
+from about import About
 import os
 
 
@@ -95,15 +96,17 @@ def on_merge_click():
     messagebox.showinfo("Success", "Models merged successfully!")
 
 
-def create_file_input(row, label_text, browse_func, bind_right_click=True):
-    label = ttk.Label(root, text=label_text)
+def create_file_input(row, label_text, browse_func, master, bind_right_click=True):
+    label = ttk.Label(master, text=label_text)
     label.grid(column=0, row=row)
-    entry = ttk.Entry(root)
-    entry.grid(column=1, row=row)
-    if bind_right_click:
-        entry.bind("<Button-3>", show_context_menu)  # Bind right-click event
-    browse_button = ttk.Button(root, text="Browse", command=lambda: browse_func(entry))
+    entry = ttk.Entry(master)
+    entry.grid(column=1, row=row, sticky=(tk.W, tk.E))
+    browse_button = ttk.Button(
+        master, text="Browse", command=lambda: browse_func(entry)
+    )
     browse_button.grid(column=2, row=row)
+    if bind_right_click:
+        entry.bind("<Button-3>", lambda event: entry.delete(0, tk.END))
     return entry
 
 
@@ -150,108 +153,114 @@ def show_context_menu(event):
 
 # Create the main window
 root = tk.Tk()
-root.title("Sd-Meh GUI")
-root.resizable(False, False)  # Disable resizing the window
+notebook = ttk.Notebook(root)
+app_frame = ttk.Frame(notebook)
+notebook.add(app_frame, text="App")
+notebook.pack(fill="both", expand=True)
 
-# Add input fields, labels, and other widgets
+grid_frame = ttk.Frame(app_frame)
+grid_frame.pack()
+
+root.title("Sd-Meh GUI")
+root.resizable(False, False)
+
 row = 0
 
-model_a_entry = create_file_input(row, "Model A", browse_model)
+model_a_entry = create_file_input(row, "Model A", browse_model, grid_frame)
 row += 1
 
-model_b_entry = create_file_input(row, "Model B", browse_model)
+model_b_entry = create_file_input(row, "Model B", browse_model, grid_frame)
 row += 1
 
-model_c_entry = create_file_input(row, "Model C", browse_model)
+model_c_entry = create_file_input(row, "Model C", browse_model, grid_frame)
 row += 1
 
-merging_method_label = ttk.Label(root, text="Merging Method")
-merging_method_label.grid(column=0, row=row)
+merging_method_label = ttk.Label(grid_frame, text="Merging Method")
+merging_method_label.grid(column=0, row=row, padx=(10, 0))
 merging_method_var = tk.StringVar()
 merging_method_combobox = ttk.Combobox(
-    root,
+    grid_frame,
     textvariable=merging_method_var,
     values=config.merging_methods,
 )
-merging_method_combobox.grid(column=1, row=row)
+merging_method_combobox.grid(column=1, row=row, padx=(0, 10))
 merging_method_combobox.current(0)
 
 row += 1
 
-precision_label = ttk.Label(root, text="Precision")
-precision_label.grid(column=0, row=row)
+precision_label = ttk.Label(grid_frame, text="Precision")
+precision_label.grid(column=0, row=row, padx=(10, 0))
 precision_var = tk.IntVar(value=16)
 precision_combobox = ttk.Combobox(
-    root, textvariable=precision_var, values=config.precisions
+    grid_frame, textvariable=precision_var, values=config.precisions
 )
-precision_combobox.grid(column=1, row=row)
+precision_combobox.grid(column=1, row=row, padx=(0, 10))
 precision_combobox.current(0)
 
 row += 1
 
-output_path_entry = create_file_input(row, "Output File", browse_output)
+output_path_entry = create_file_input(row, "Output File", browse_output, grid_frame)
 
 row += 1
 
-output_format_label = ttk.Label(root, text="Output Format")
-output_format_label.grid(column=0, row=row)
+output_format_label = ttk.Label(grid_frame, text="Output Format")
+output_format_label.grid(column=0, row=row, padx=(10, 0))
 output_format_var = tk.StringVar()
 output_format_combobox = ttk.Combobox(
-    root, textvariable=output_format_var, values=config.output_formats
+    grid_frame, textvariable=output_format_var, values=config.output_formats
 )
-output_format_combobox.grid(column=1, row=row)
+output_format_combobox.grid(column=1, row=row, padx=(0, 10))
 output_format_combobox.current(0)
 
-# Set the initial values for the dropdowns based on the loaded settings
 merging_method_combobox.current(config.merging_method_index)
 precision_combobox.current(config.precision_index)
 output_format_combobox.current(config.output_format_index)
 
 row += 1
 
-weights_alpha_label = ttk.Label(root, text="Weights Alpha")
-weights_alpha_label.grid(column=0, row=row)
-weights_alpha_entry = ttk.Entry(root)
-weights_alpha_entry.grid(column=1, row=row)
+weights_alpha_label = ttk.Label(grid_frame, text="Weights Alpha")
+weights_alpha_label.grid(column=0, row=row, padx=(10, 0))
+weights_alpha_entry = ttk.Entry(grid_frame)
+weights_alpha_entry.grid(column=1, row=row, padx=(0, 10))
 weights_alpha_entry.bind("<Button-3>", show_context_menu)
 
 row += 1
 
-base_alpha_label = ttk.Label(root, text="Base Alpha")
-base_alpha_label.grid(column=0, row=row)
-base_alpha_entry = ttk.Entry(root)
-base_alpha_entry.grid(column=1, row=row)
+base_alpha_label = ttk.Label(grid_frame, text="Base Alpha")
+base_alpha_label.grid(column=0, row=row, padx=(10, 0))
+base_alpha_entry = ttk.Entry(grid_frame)
+base_alpha_entry.grid(column=1, row=row, padx=(0, 10))
 base_alpha_entry.insert(0, "0.0")
 base_alpha_entry.bind("<Button-3>", show_context_menu)
 
 row += 1
 
-weights_beta_label = ttk.Label(root, text="Weights Beta")
-weights_beta_label.grid(column=0, row=row)
-weights_beta_entry = ttk.Entry(root)
-weights_beta_entry.grid(column=1, row=row)
+weights_beta_label = ttk.Label(grid_frame, text="Weights Beta")
+weights_beta_label.grid(column=0, row=row, padx=(10, 0))
+weights_beta_entry = ttk.Entry(grid_frame)
+weights_beta_entry.grid(column=1, row=row, padx=(0, 10))
 weights_beta_entry.bind("<Button-3>", show_context_menu)
 
 row += 1
 
-base_beta_label = ttk.Label(root, text="Base Beta")
-base_beta_label.grid(column=0, row=row)
-base_beta_entry = ttk.Entry(root)
-base_beta_entry.grid(column=1, row=row)
+base_beta_label = ttk.Label(grid_frame, text="Base Beta")
+base_beta_label.grid(column=0, row=row, padx=(10, 0))
+base_beta_entry = ttk.Entry(grid_frame)
+base_beta_entry.grid(column=1, row=row, padx=(0, 10))
 base_beta_entry.insert(0, "0.0")
 base_beta_entry.bind("<Button-3>", show_context_menu)
 
 row += 1
 
-buttons_frame = ttk.Frame(root)
-buttons_frame.grid(column=0, row=row, columnspan=3)
-
-version_button = ttk.Button(buttons_frame, text="?", command=show_version_info, width=3)
-version_button.pack(side="left", padx=(0, 0))
+buttons_frame = ttk.Frame(grid_frame)
+buttons_frame.grid(column=0, row=row, columnspan=3, pady=(10, 0))
 
 merge_button = ttk.Button(buttons_frame, text="Merge Models", command=on_merge_click)
-merge_button.pack(side="left")
+merge_button.pack(side="left", padx=(10, 0))
 
+about = About()
+about_frame = about.create_about_frame(notebook)
+notebook.add(about_frame, text="About")
 
 root.protocol("WM_DELETE_WINDOW", on_close)
 root.mainloop()
