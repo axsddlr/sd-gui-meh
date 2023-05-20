60fbabda1d525f9a12365c5b9b0c847922b5ef97
Andre Saddler <dresadd09691@gmail.com>
2023-05-20 02:39:00 -0400

    refactor(app.py): remove Config class from app.py and import it from settings.py refactor(app.py): remove load_settings and save_settings methods from Config class and move them to settings.py The Config class has been moved to settings.py to improve code organization and separation of concerns. The load_settings and save_settings methods have also been moved to settings.py to keep the Config class focused on its responsibilities.

--- app.py
+++ app.py
@@ -1,52 +1,8 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from sd_meh.merge import NUM_TOTAL_BLOCKS, merge_models, save_model
+from settings import Config
 import os
-import json
-
-
-class Config:
-    def __init__(self):
-        self.merging_methods = [
-            "weighted_sum",
-            "add_difference",
-            "weighted_subtraction",
-            "sum_twice",
-            "triple_sum",
-            "tensor_sum",
-        ]
-        self.precisions = [16, 32]
-        self.output_formats = ["safetensors", "ckpt"]
-
-        # Set default indices
-        self.merging_method_index = 0
-        self.precision_index = 0
-        self.output_format_index = 0
-
-    def load_settings(self, file_path):
-        try:
-            with open(file_path, "r") as f:
-                settings = json.load(f)
-                # Load the settings into the config instance
-                for key, value in settings.items():
-                    setattr(self, key, value)
-        except FileNotFoundError:
-            # If the settings file doesn't exist, create a default one
-            self.save_settings(file_path)
-        except Exception as e:
-            messagebox.showerror("Error", f"Error loading settings: {e}")
-
-    def save_settings(self, file_path):
-        settings = {
-            "merging_method_index": merging_method_combobox.current(),
-            "precision_index": precision_combobox.current(),
-            "output_format_index": output_format_combobox.current(),
-        }
-        try:
-            with open(file_path, "w") as f:
-                json.dump(settings, f, indent=4)
-        except Exception as e:
-            messagebox.showerror("Error", f"Error saving settings: {e}")
 
 
 config = Config()
@@ -166,7 +122,12 @@ def browse_output(entry):
 
 def on_close():
     # Perform any necessary cleanup here
-    config.save_settings("settings.json")
+    settings = {
+        "merging_method_index": merging_method_combobox.current(),
+        "precision_index": precision_combobox.current(),
+        "output_format_index": output_format_combobox.current(),
+    }
+    config.save_settings("settings.json", settings)
     root.destroy()
 
 
